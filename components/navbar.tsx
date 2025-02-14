6506d8751270c471a4805bd78b89f84ffe0a8fcd
Rose Trindade <rosemtrindade@gmail.com>
2025-02-14 17:12:41 -0300

    navbar clenup

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -14,7 +14,6 @@ import { siteConfig } from "@/config/site";
 import { ThemeSwitch } from "@/components/theme-switch";
 
 export const Navbar = () => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const searchInput = (
     <Input
       aria-label="Search"
@@ -39,58 +38,21 @@ export const Navbar = () => {
   return (
     <NextUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
-        {/* <NavbarBrand as="li" className="gap-3 max-w-fit">
-          <NextLink className="flex justify-start items-center gap-1" href="/">
-            <Logo />
-            <p className="font-bold text-inherit">ACME</p>
-          </NextLink>
-        </NavbarBrand> */}
-        {/* <ul className="hidden lg:flex gap-4 justify-start ml-2">
-          {siteConfig.navItems.map((item) => (
-            <NavbarItem key={item.href}>
-              <NextLink
-                className={clsx(
-                  linkStyles({ color: "foreground" }),
-                  "data-[active=true]:text-primary data-[active=true]:font-medium",
-                )}
-                color="foreground"
-                href={item.href}
-              >
-                {item.label}
-              </NextLink>
-            </NavbarItem>
-          ))}
-        </ul> */}
       </NavbarContent>
 
       <NavbarContent
         className="hidden sm:flex basis-1/5 sm:basis-full"
         justify="end"
       >
-        {/* <NavbarItem className="hidden sm:flex gap-2">
-          <Link isExternal aria-label="Github" href={siteConfig.links.github}>
-            <FaGithub className="" />
-          </Link>
-          </NavbarItem> */}
-        {/* <NavbarItem className="hidden lg:flex">{searchInput}</NavbarItem> */}
         <NavbarItem className="hidden md:flex gap-2 text-default-500 items-center">
           <ThemeSwitch />
           <FaCartPlus />
         </NavbarItem>
       </NavbarContent>
 
-      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal aria-label="Github" href={siteConfig.links.github}>
-          <FaGithub className="text-default-500" />
-        </Link>
-        <ThemeSwitch />
-        {/* <NavbarMenuToggle /> */}
-      </NavbarContent>
-      <NavbarContent className="sm:hidden basis-1 pl-4" justify="end">
-        <Link isExternal aria-label="Github" href={siteConfig.links.github}>
+      <NavbarContent className="sm:hidden text-default-500" justify="end">
+          <ThemeSwitch />
           <FaCartPlus />
-        </Link>
-        {/* <NavbarMenuToggle /> */}
       </NavbarContent>
     </NextUINavbar>
   );
